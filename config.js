8d3386f877119a3183922321d400910c14f5b3ef
raruto2023-sketch <raruto.2023@gmail.com>
2026-07-30 02:46:09 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+// Lait Divin online configuration
 window.LAIT_DIVIN_CONFIG = {
   SUPABASE_URL: "https://joowzytokjrwmlnglbas.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_81u53Z8UNHfAc6ivtBLJ1w_rnHh9qWv",
