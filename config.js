6ebc6ca8e2f6533a4187e13a1b608783c3c544fc
raruto2023-sketch <raruto.2023@gmail.com>
2026-07-30 02:38:18 +0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.LAIT_DIVIN_CONFIG = {
-  SUPABASE_URL: "https://joowzytokjrwmlnglbas.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_81u53Z8UNHfAc6ivtBLJ1w_rnHh9qWv",
-  ADMIN_EMAIL: "admin@lait-divin.local",
-  LOGIN_DOMAIN: "lait-divin.local"
-};
